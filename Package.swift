17ce2d4bbbb2784b59b2305495a351593ada7bf8
Justin Uberti <justin@uberti.name>
2022-04-12 17:41:05 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/alphaexplorationco/webrtc-pods/releases/download/97.0.0/WebRTC-M97.xcframework.zip",
+            url: "https://github.com/juberti/webrtc-pods/releases/download/97.0.0/WebRTC-M97.xcframework.zip",
             checksum: "e8278149b4bfeed6a5289b44f20dbb977448d963f6ae47444040cf79bd58e856"
         ),
     ]
